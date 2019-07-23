cb37b6701920be4e1cdf7e082e31f9ebe83aa1a3
Francisco Ramirez <FranciscoJ.RamirezLuna@outlook.com>
2019-07-23 11:42:23 -0600

    tarea: validar que la columna max_stundents  no este vacia

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
     validates_presence_of :name, :start_date, :end_date
     has_and_belongs_to_many :users, -> { distinct }
 
+    validates :max_students, presence: true, uniqueness: false
+
     
 
     def is_full?
